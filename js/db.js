a8dd0d7de5e19e81e0da28e0eb514ed9ac90569f
Paul Pichon F <paul10_barca@hotmail.com>
2022-12-12 15:30:50 -0600

    Inicio de proyecto

---
+++ js/db.js
@@ -0,0 +1,12 @@
+const negocios = [
+    {
+        municipio : 'Tlaxcala',
+        tipoComida: 'Mexicana',
+        disponinilidad: 'Abierto',
+        rangoPrecioMinimo: 100,
+        rangoPrecioMaximo: 800,
+        metodoPago: 'Efectivo',
+        calificacion: 4.5,
+        estacionamiento: 'No'
+    }
+];
