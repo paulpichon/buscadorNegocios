04d0c3faad8b7f9815d4930320fa85d6b75c0303
Paul Pichon F <paul10_barca@hotmail.com>
2022-12-14 23:08:08 -0600

    Funcion para mostrar los autos

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-console.log(negocios);
+//constantes
+const resultados = document.querySelector('#resultados');
+//listeners
+eventListeners();
+function eventListeners() {
+    //mostrar negocios
+    document.addEventListener('DOMContentLoaded', mostrarNegocios)
+}
+
+//funciones
+//mostrar negocios
+function mostrarNegocios() {
+    negocios.forEach( negocio => {
+        console.log( negocio );
+        //destructuring
+        const { calificacion, disponibilidad, estacionamiento, metodoPago, municipio, nombre, rangoPrecioMaximo, rangoPrecioMinimo, tipoComida } = negocio;
+        //construir el HTML
+        const pNegocio = document.createElement('p');
+        //styles
+        pNegocio.classList.add('border-2', 'border-indigo-200', 'p-3');
+        //textcontent
+        pNegocio.textContent = `Nombre de negocio: ${nombre} - ${municipio} - ${ disponibilidad } - Calificacion: ${calificacion}/5 - Estacionamiento: ${estacionamiento} - Metodo de pago: ${metodoPago} - Tipo de comida: ${tipoComida} - Rangos de precios: $${rangoPrecioMinimo} - $${rangoPrecioMaximo} `;
+
+        //renderizar
+        resultados.appendChild( pNegocio );
+
+    });
+}
